6d8ab0f3a57d230841486840231836d6daa90d10
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-10-07 12:58:01 +0200

    Styling intro section

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -2,7 +2,8 @@ function IntroSection() {
     return (
         <section className="intro">
             <header className="intro__header header">
-                <a href="" className="header__logo logo"><img src="../../src/images/Logo.svg" alt="Travelian logo" className="logo__img"/></a>
+                <a href="" className="header__logo logo"><img src="../../src/images/Logo.svg" alt="Travelian logo"
+                                                              className="logo__img"/></a>
                 <nav className="header__menu main-menu">
                     <a href="" className="main-menu__item main-menu__item--active">Home</a>
                     <a href="" className="main-menu__item">Explore</a>
@@ -19,6 +20,34 @@ function IntroSection() {
                 <h1 className="lead__hdl">Start your unforgettable<br/> journey with us.</h1>
                 <p className="lead__text">The best travel for your journey begins now</p>
             </div>
+            <form action="" className="search">
+                <div className="search__wrapper">
+                    <div className="search__group">
+                        <label htmlFor="destination" className="search__lbl">Destination</label>
+                        <input type="text" id="destination" name="destination" className="search__input"
+                               placeholder="Dubai"/>
+                    </div>
+                    <div className="search__group">
+                        <label htmlFor="person" className="search__lbl">Person</label>
+                        <select name="person" id="person" className="search__input search__input--select">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                    <div className="search__group">
+                        <label htmlFor="checkin" className="search__lbl">Check in</label>
+                        <input type="date" id="checkin" name="checkin" className="search__input search__input--check"/>
+                    </div>
+                    <div className="search__group">
+                        <label htmlFor="checkout" className="search__lbl">Check out</label>
+                        <input type="date" id="checkout" name="checkout" className="search__input search__input--check"/>
+                    </div>
+                    <button type="submit" className="search__btn">Book<br/>Now</button>
+                </div>
+            </form>
         </section>
     );
 }
