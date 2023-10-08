22a008ce4961993313897c252583bb9365dc7004
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-10-08 19:17:36 +0200

    Styling destination section

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -7,15 +7,14 @@ function Destination() {
                     <p className="leadDes__text">Most popular destinations around the world, from historical places to natural wonders.</p>
                 </div>
                 <div className="content__btnDes btnsDes">
-                    <button className="btnDes__btnLeft"><img src="" alt=""/></button>
-                    <button className="btnDes__btnRight"><img src="" alt=""/></button>
+                    <button className="btnsDes__btnLeft"></button>
+                    <button className="btnsDes__btnRight"></button>
                 </div>
             </div>
             <div className="destinations__imgsDes imgsDes">
-                <img className="imgDes__img" src="" alt=""/>
-                <img className="imgDes__img" src="" alt=""/>
-                <img className="imgDes__img" src="" alt=""/>
-                <img className="imgDes__img" src="" alt=""/>
+                <img className="imgsDes__img" src="../../src/images/Col-block.jpg" alt=""/>
+                <img className="imgsDes__img" src="../../src/images/Col-block2.jpg" alt=""/>
+                <img className="imgsDes__img" src="../../src/images/Col-block3.jpg" alt=""/>
             </div>
         </section>
     );
