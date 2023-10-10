2dbc375321f67c49e4b045f9ffc84394d3c17452
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-10-10 20:35:25 +0200

    Create our blog component, add html and sass structure

---
+++ src/components/OurBlog.jsx
@@ -0,0 +1,22 @@
+function OurBlog() {
+    return (
+        <section className="ourBlog">
+            <div className="ourBlog__titleBlog titleBlog">
+                <div className="titleBlog__leadBlog leadBlog">
+                    <h2 className="leadBlog__hdlBlog">Our Blog</h2>
+                    <p className="leadBlog__textBlog">An insight the incredible experience in the world</p>
+                </div>
+            </div>
+            <div className="contentBlog">
+                <img className="contentBlog__imgBlog" src="" alt=""/>
+                <div className="descriptionBlog">
+                    <h3 className="descriptionBlog__hdlBlog"></h3>
+                    <p className="descriptionBlog__txtBlog"></p>
+                    <a className="descriptionBlog__linkBlog"></a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default OurBlog;
